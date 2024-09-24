614846426be1bf4c56601ff2ff36b1fabe353112
Valerii Omelnyk <valeraaomelnik@gmail.com>
2024-09-24 04:55:17 +0300

    make build

---
+++ index.e31fcc6b.js
@@ -0,0 +1,2 @@
+!function e(n,r){if(r){var a=document.createElement("ul");for(var t in r){var i=document.createElement("li");i.innerHTML=t,a.append(i),e(i,r[t])}n.append(a)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.e31fcc6b.js.map
